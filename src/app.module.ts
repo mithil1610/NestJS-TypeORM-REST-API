0985fddcf8dda8e88797190c69f7482a364cc9cf
Mithil Patel <mithilpatel140@gmail.com>
2020-09-24 18:06:21 +0530

    Logging feature added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,48 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { WinstonModule } from 'nest-winston';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpErrorFilter } from './shared/http-error.filter';
 import { User } from './users/entity/user.entity';
 import { UsersModule } from './users/users.module';
+import * as winston from 'winston';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'mithil',
-    database: 'test',
-    entities: [User],
-    synchronize: true,
-  })],
+  imports: [
+    UsersModule, 
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'mithil',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }), 
+    WinstonModule.forRoot({
+      level: 'info',
+      format: winston.format.json(),
+      defaultMeta: { service: 'user-service' },
+      transports: [
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' }),
+      ],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, 
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule { }
